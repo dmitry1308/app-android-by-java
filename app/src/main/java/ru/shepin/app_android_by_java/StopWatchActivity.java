2de8e2b22b8cf4963fef8884023be8fa0b17dbae
Schepin Dmitriy <bright1308@gmail.com>
2021-10-29 18:15:06 +0300

    create activity

---
+++ app/src/main/java/ru/shepin/app_android_by_java/StopWatchActivity.java
@@ -0,0 +1,21 @@
+package ru.shepin.app_android_by_java;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StopWatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClickStart(View view) {
+    }
+}
