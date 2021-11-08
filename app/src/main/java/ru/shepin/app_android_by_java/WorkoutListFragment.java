06ce65c6a830cf057acf0207328752b01859c12d
Schepin Dmitriy <bright1308@gmail.com>
2021-11-08 12:06:15 +0300

    workWithFragment: add ListFragment

--- app/src/main/java/ru/shepin/app_android_by_java/WorkoutListFragment.java
+++ app/src/main/java/ru/shepin/app_android_by_java/WorkoutListFragment.java
@@ -3,19 +3,20 @@ package ru.shepin.app_android_by_java;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class WorkoutListFragment extends Fragment {
+public class WorkoutListFragment extends ListFragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workout_list, container, false);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
