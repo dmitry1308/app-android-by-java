2bd8e614b1afd95e0f467dd8d482eb13ad8619cb
Schepin Dmitriy <bright1308@gmail.com>
2021-11-08 21:50:19 +0300

    app_withFragment: fill WorkoutListFragment

--- app/src/main/java/ru/shepin/app_android_by_java/WorkOut.java
+++ app/src/main/java/ru/shepin/app_android_by_java/WorkOut.java
@@ -16,7 +16,7 @@ public class WorkOut {
 
     public static final WorkOut[] workouts = {
             new WorkOut("The Limb Loosener", "5  Handstand push-ups\n10 1-legged squats"),
-            new WorkOut("Core agony", "5  Handstand push-ups\n10 1-legged squats"),
-            new WorkOut("The Wimp Special", "5  Handstand push-ups\n10 1-legged squats")
+            new WorkOut("Core agony", "1  Handstand push-ups\n10 1-legged squats"),
+            new WorkOut("The Wimp Special", "3  Handstand push-ups\n10 1-legged squats")
     };
 }
